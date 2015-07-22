cc55e0112255c374b2d87a1268e09f8011e9d76d
Anshul Verma <anshul.verma86@gmail.com>
2015-07-22 09:10:59 -0700

    add test for negative path of event marshalling

---
+++ events/src/test/java/com/nuaavee/skybridge/events/UnknownEventMarshallerTest.java
@@ -0,0 +1,29 @@
+package com.nuaavee.skybridge.events;
+
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnknownEventMarshallerTest {
+
+  @Test
+  public void testEmptyJson() throws IOException{
+    try {
+      new EventMarshaller().unmarshall("{}");
+      Assert.fail("expected " + UnknownEventTypeException.class + " to be thrown");
+    } catch (UnknownEventTypeException e) {
+      Assert.assertEquals("event type in error should be null", null, e.getEventType());
+    }
+  }
+
+  @Test
+  public void testInvalidEventJson() throws IOException {
+    try {
+      new EventMarshaller().unmarshall("{\"eventType\":\"this_is_unknown\"}");
+      Assert.fail("expected " + UnknownEventTypeException.class + " to be thrown");
+    } catch (UnknownEventTypeException e) {
+      Assert.assertEquals("event type in error should be null", "this_is_unknown", e.getEventType());
+    }
+  }
+
+}
